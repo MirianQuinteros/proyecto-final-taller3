31f310ee4b8d6d4a8f297e7437468bb15329fa3a
MirianQuinteros <mirian.quinteros@avantrip.com>
2016-08-16 00:38:46 -0300

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,3 @@
+columnHeaders=['Id', 'Descripcion', 'Voltaje (KV)', 'Señal', 'F(EHD) KHz', 'F (forz) Hz', 'DC %','Duracion (Seg)', 'Cámara', 'Iny. Humo']
+
+columnFields=['id', 'descr', 'voltage', 'signalType', 'fehd', 'fforz', 'dc', 'duration', 'camera', 'smoke']
