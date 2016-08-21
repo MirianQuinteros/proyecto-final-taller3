ec3bf5c77189110a8630a1ebeebb13dbe4a36b79
MirianQuinteros <mirian.quinteros@avantrip.com>
2016-08-21 14:03:32 -0300

    Delete constants.py

--- constants.py
+++
@@ -1,3 +0,0 @@
-columnHeaders=['Id', 'Descripcion', 'Voltaje (KV)', 'Señal', 'F(EHD) KHz', 'F (forz) Hz', 'DC %','Duracion (Seg)', 'Cámara', 'Iny. Humo']
-
-columnFields=['id', 'descr', 'voltage', 'signalType', 'fehd', 'fforz', 'dc', 'duration', 'camera', 'smoke']
